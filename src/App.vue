ccdb436ddfdbe95ac698e5e5d83924fb1c97cc78
heejin <heejin2dat@naver.com>
2024-11-17 21:57:47 +0900

    11/17

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/test">Test</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/render">Extra</router-link> |
     <router-link to="/show">Show</router-link>
-    <!-- <router-link to="/string">String</router-link>  -->
+    
     
     
 
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>Hello Vue</h1>
+  <div>
+    <nav>
+      <a v-for="(menu,i) in menuLists" :key="i" :href="menu.link">{{ menu.text }}</a>
+    </nav>
+
+
+  </div>
 </template>
+<script>
+
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {
+      menuLists: [
+        { text: '메뉴1', link: 'render'},
+        { text: '메뉴2', link: 'show'},
+        { text: '메뉴3', link: 'watch'},
+        { text: '메뉴4', link: 'about'},
+      ]
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    
+  }
+}
+</script>
